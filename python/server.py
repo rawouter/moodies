10252be1c5e2bffc901b5cab523b862c2224e0ce
Raphael Wouters <rawouter@gmail.com>
2015-03-15 11:23:35 +0100

    Handling connections and disconnections, plus a bit or reformating

--- python/server.py
+++ python/server.py
@@ -1,9 +1,11 @@
+from collections import namedtuple
 import json
 import logging
-from PythonPusherClient import pusherclient
 import sys
 import time
 import types
+
+from PythonPusherClient import pusherclient
 
 # Configuration
 APPKEY = '2c987384b72778026687'
@@ -18,16 +20,17 @@ USERDATA = {
 
 
 class MoodiesServer:
+
     """
     Main server class, will connect to pusher, listen to messages, compute them and answer.
     """
-
 
     def __init__(self):
         self.logger = logging.getLogger('moodies.Server')
         self.killed = False
         self.pusher = pusherclient.Pusher(APPKEY, secret=SECRET, user_data=USERDATA)
         self.channels = {}
+        self.users = {}
 
     def run_forever(self):
         """
@@ -38,6 +41,7 @@ class MoodiesServer:
         while not self.killed:
             time.sleep(1)
 
+
     def _connect_to_pusher(self):
         """
         Establish the connection to pusher, and configure the callback function
@@ -46,6 +50,7 @@ class MoodiesServer:
         self.pusher.connection.bind('pusher:connection_established', self._callback_connection_estabished)
         self.pusher.connection.bind('default', self._callback_default)
         self.pusher.connect()
+
 
     def _callback_connection_estabished(self, data):
         """
@@ -62,7 +67,8 @@ class MoodiesServer:
             self._setup_mood_channels_callbacks(moodies_channel.pusher_channel)
 
     def _callback_default(self, channel_name, msg):
-        self.logger.debug("DEFAULT HANDLER: {} - {}".format(channel_name, msg))
+        self.logger.warn("!!! DEFAULT HANDLER !!! {} - {}".format(channel_name, msg))
+
 
     def _setup_config_channel_callbacks(self, pusher_channel_config):
         """
@@ -78,13 +84,26 @@ class MoodiesServer:
         pusher_channel.bind('pusher_internal:member_removed', self._callback_leaving_member)
         pusher_channel.bind('client-button-pushed', self._callback_button_pushed)
 
+
     def _callback_joining_member(self, channel_name, msg):
+        """
+        Create a new MoodiesUser and store it for the first time we see the user.
+        Append the user to the channel list of users.
+        """
         message = Message(msg)
         self.logger.info('{} entering {}'.format(message.user_id, channel_name))
+        if message.user_id not in self.users.keys():
+            self.users[message.user_id] = MoodiesUser(message.user_id)
+        if self.users[message.user_id] not in self.channels[channel_name].users:
+            self.channels[channel_name].users.append(self.users[message.user_id])
 
     def _callback_leaving_member(self, channel_name, msg):
+        """
+        Remove users from channel users list.
+        """
         message = Message(msg)
         self.logger.info('{} left {}'.format(message.user_id, channel_name))
+        self.channels[channel_name].users.remove(self.users[message.user_id])
 
     def _callback_button_pushed(self, channel_name, msg):
         message = Message(msg)
@@ -93,48 +112,65 @@ class MoodiesServer:
         #self.channels[channel_name]
 
 
-class MoodiesChannel:
+class MoodiesUser:
+
     """
-    Channel class representing a chat room (pusher channel) and it's mood (Mood class)
+    User class, store Mood per users
     """
 
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.moods = Moods()
+
+
+class MoodiesChannel:
+
+    """
+    Channel class representing a chat room (pusher channel) and it's moods (Moods class)
+    """
 
     def __init__(self, pusher_channel):
         self.logger = logging.getLogger('moodies.MoodiesChannel')
         self.pusher_channel = pusher_channel
         self.name = pusher_channel.name
-        self.mood = Mood()
+        self.moods = Moods()
+        self.users = []
 
 
-class Mood:
+class Moods:
+
     """
-    Contains mood values
+    Contains Mood values
     """
-
 
     def __init__(self):
-        self.excited = 0
-        self.nervous = 0
+        Mood = namedtuple('Mood', ['name', 'value', 'color'])
+        self.excited = Mood('excited', 0, '00FF00')
+        self.nervous = Mood('nervous', 0, 'FF0000')
 
-    def decrease_all_moods(self):
+    def decrease_all_moods(self, val):
         for key in vars(self):
             mood = self[key]
-            assert type(mood) is types.IntType, 'Mood class has a non Integer variable: {}'.format(key)
-            if mood > 0:
-                mood -= 1
+            self.decrease(mood, val)
 
-    def increase(self, mood_var):
-        if mood_var < 100:
-            mood_var += 1
+    def decrease(self, mood_var, val=10):
+        if mood_var.value > val:
+            mood_var.value -= val
         else:
-            mood_var = 100
+            mood_var.value = 0
+
+    def increase(self, mood_var, val=10):
+        if mood_var.value < (100-val):
+            mood_var.value += val
+        else:
+            mood_var.value = 100
 
 
 class Message:
-    """
-    Parse a json object
-    """
 
+    """
+    Parse a the json string received in pusher message data
+    """
 
     def __init__(self, msg):
         assert type(msg) in types.StringTypes, 'Message instance did not receive a String'
