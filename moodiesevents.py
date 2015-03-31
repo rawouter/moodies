2ab2fda99d10ae873f3c0f2b6ce8ffec0fd9684a
Raphael Wouters <rawouter@gmail.com>
2015-03-31 22:25:45 +0200

    Document events

--- moodiesevents.py
+++ moodiesevents.py
@@ -1,4 +1,14 @@
+"""
+Defines the event strings that can this server will send or listen to in the pusher channel.
+
+Clients events must be prefixed by client-
+https://pusher.com/docs/client_api_guide/client_events
+"""
+
+# Events received by this server
 BUTTON_PUSHED = 'client-button-pushed'
+
+# Events sent by this server
 CHANGE_COLOR = 'client-new-color'
 DISPLAY_TEXT = 'client-text-message'
 PLAY_MELODY = 'client-play-melody'
